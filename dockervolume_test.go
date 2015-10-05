bf0ea47bb26d8525b480c829222d7a05e86292dc
Peter Edge <peter.edge@gmail.com>
2015-10-05 00:20:45 -0700

    more testing

--- dockervolume_test.go
+++ dockervolume_test.go
@@ -26,11 +26,9 @@ func TestBasic(t *testing.T) {
 func testBasic(t *testing.T, fakeVolumeDriver *fakeVolumeDriver, client VolumeDriverClient) {
 	err := client.Create("foo", map[string]string{"key": "value", "uint64": "1234"})
 	require.NoError(t, err)
-	volumes, err := client.ListVolumes()
-	require.NoError(t, err)
-	require.Len(t, volumes, 1)
-	require.Equal(
+	requireVolumesEqual(
 		t,
+		client,
 		&Volume{
 			Name: "foo",
 			Opts: map[string]string{
@@ -39,17 +37,14 @@ func testBasic(t *testing.T, fakeVolumeDriver *fakeVolumeDriver, client VolumeDr
 			},
 			Mountpoint: "",
 		},
-		volumes[0],
 	)
 	fakeVolumeDriver.requireStatusEquals("foo", fakeStatusCreate)
 	mountpoint, err := client.Mount("foo")
 	require.NoError(t, err)
 	require.Equal(t, "/mnt/foo", mountpoint)
-	volumes, err = client.ListVolumes()
-	require.NoError(t, err)
-	require.Len(t, volumes, 1)
-	require.Equal(
+	requireVolumesEqual(
 		t,
+		client,
 		&Volume{
 			Name: "foo",
 			Opts: map[string]string{
@@ -58,9 +53,49 @@ func testBasic(t *testing.T, fakeVolumeDriver *fakeVolumeDriver, client VolumeDr
 			},
 			Mountpoint: "/mnt/foo",
 		},
-		volumes[0],
 	)
 	fakeVolumeDriver.requireStatusEquals("foo", fakeStatusMount)
+	err = client.Unmount("foo")
+	require.NoError(t, err)
+	requireVolumesEqual(
+		t,
+		client,
+		&Volume{
+			Name: "foo",
+			Opts: map[string]string{
+				"key":    "value",
+				"uint64": "1234",
+			},
+			Mountpoint: "",
+		},
+	)
+	fakeVolumeDriver.requireStatusEquals("foo", fakeStatusUnmount)
+	err = client.Remove("foo")
+	require.NoError(t, err)
+	requireVolumesEqual(t, client)
+}
+
+func requireVolumesEqual(t *testing.T, client VolumeDriverClient, expected ...*Volume) {
+	volumes, err := client.ListVolumes()
+	require.NoError(t, err)
+	require.Equal(t, len(expected), len(volumes))
+	nameToExpected := make(map[string]*Volume)
+	nameToActual := make(map[string]*Volume)
+	for _, volume := range expected {
+		nameToExpected[volume.Name] = volume
+	}
+	for _, volume := range volumes {
+		nameToActual[volume.Name] = volume
+	}
+	require.Equal(t, len(nameToExpected), len(nameToActual))
+	for name, expected := range nameToExpected {
+		actual, ok := nameToActual[name]
+		require.True(t, ok)
+		require.Equal(t, expected, actual)
+		volume, err := client.GetVolume(name)
+		require.NoError(t, err)
+		require.Equal(t, expected, volume)
+	}
 }
 
 func runTest(
@@ -87,54 +122,57 @@ func runTest(
 	)
 }
 
-type fakeVolume struct {
-	name       string
-	opts       Opts
-	mountpoint string
-	fakeStatus fakeStatus
-}
-
 type fakeVolumeDriver struct {
 	t                *testing.T
-	nameToFakeVolume map[string]*fakeVolume
+	nameToFakeVolume map[string]*Volume
+	nameToFakeStatus map[string]fakeStatus
 }
 
 func newFakeVolumeDriver(t *testing.T) *fakeVolumeDriver {
 	return &fakeVolumeDriver{
 		t,
-		make(map[string]*fakeVolume),
+		make(map[string]*Volume),
+		make(map[string]fakeStatus),
 	}
 }
 
 func (v *fakeVolumeDriver) Create(name string, opts Opts) error {
-	v.nameToFakeVolume[name] = &fakeVolume{
-		name:       name,
-		opts:       opts,
-		mountpoint: "",
-		fakeStatus: fakeStatusCreate,
+	v.nameToFakeVolume[name] = &Volume{
+		Name:       name,
+		Opts:       testOpts(opts),
+		Mountpoint: "",
 	}
+	v.nameToFakeStatus[name] = fakeStatusCreate
 	return nil
 }
 
 func (v *fakeVolumeDriver) Remove(name string, opts Opts, mountpoint string) error {
-	v.nameToFakeVolume[name].fakeStatus = fakeStatusRemove
+	v.nameToFakeStatus[name] = fakeStatusRemove
 	return nil
 }
 
 func (v *fakeVolumeDriver) Mount(name string, opts Opts) (string, error) {
 	mountpoint := fmt.Sprintf("/mnt/%s", name)
-	v.nameToFakeVolume[name].mountpoint = mountpoint
-	v.nameToFakeVolume[name].fakeStatus = fakeStatusMount
+	v.nameToFakeVolume[name].Mountpoint = mountpoint
+	v.nameToFakeStatus[name] = fakeStatusMount
 	return mountpoint, nil
 }
 
 func (v *fakeVolumeDriver) Unmount(name string, opts Opts, mountpoint string) error {
-	v.nameToFakeVolume[name].fakeStatus = fakeStatusUnmount
+	v.nameToFakeVolume[name].Mountpoint = mountpoint
+	v.nameToFakeStatus[name] = fakeStatusUnmount
 	return nil
 }
 
-func (v *fakeVolumeDriver) requireStatusEquals(name string, fakeStatus fakeStatus) {
-	fakeVolume, ok := v.nameToFakeVolume[name]
+func (v *fakeVolumeDriver) requireStatusEquals(name string, expected fakeStatus) {
+	fakeStatus, ok := v.nameToFakeStatus[name]
 	require.True(v.t, ok)
-	require.Equal(v.t, fakeStatus, fakeVolume.fakeStatus)
+	require.Equal(v.t, expected, fakeStatus)
+}
+
+func testOpts(o Opts) map[string]string {
+	if o == nil {
+		return nil
+	}
+	return o.(*opts).m
 }
